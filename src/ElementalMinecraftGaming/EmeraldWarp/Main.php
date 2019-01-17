31c0209ac631e5f6fb570948c0fe32d0a031fa55
MDC <mathewpsutherland@gmail.com>
2019-01-17 12:34:51 +0000

    Update Main.php

--- src/ElementalMinecraftGaming/EmeraldWarp/Main.php
+++ src/ElementalMinecraftGaming/EmeraldWarp/Main.php
@@ -17,42 +17,54 @@ use pocketmine\utils\TextFormat;
 class Main extends PluginBase implements listener {
 	
 	private $config;
-	
-	public function onEnable() {
-    $this->getLogger()->info(TextFormat::GREEN . "Created by MrDevCat -Discord- ");
-		@mkdir($this->getDataFolder());
-		$this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML, array());
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-	}
-	
-	public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
-		if ($sender->hasPermission("emerald.setwarp")) {
-			if (strtolower($command->getName()) == "emeraldsetwarp") {
-				if ($sender instanceof Player) {
-					if (isset($args[0])) {
-					$sender->sendMessage(TextFormat::GOLD . "Setting warp");
-					$leveled = $player->getLevel()->getFolderName();
-					$z = $player->getZ();
-					$x = $player->getX();
-					$y = $player->getY();
-						$warp = $args[0];
-					$config = $plugin->getConfig();
-                                        $config->set([$leveled, $x, $y, $z,"entry 2"],$warp);
-                                        $config->save();
-					$sender->sendMessage(TextFormat::GOLD . "Warp set at: $leveled, $x, $y, $z");
-					return true;
-				} else {
-					sender->sendMessage(TextFormat::RED . "Incorrect usage or privlages!");
-					return false;
-				}
-			}
-			
-			if ($sender->hasPermission("emerald.warp")) {
-				if (strtolower($command->getName()) == "emeraldwarp") {
-					if ($sender instanceof Player) {
-						$sender->sendMessage(TextFormat::GOLD . "Warping!");
-						//ToDo
-						return true;
-					} else {
-						$sender->sendMessage(TextFormat::RED . "Potty trouble!");
-						return false;
+
+    public function onEnable() {
+        $this->getLogger()->info(TextFormat::GREEN . "Created by MrDevCat -Discord- ");
+        @mkdir($this->getDataFolder());
+        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML, array());
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        if ($sender->hasPermission("emerald.setwarp")) {
+            if (strtolower($command->getName()) == "emeraldsetwarp") {
+                if ($sender instanceof Player) {
+                    if (isset($args[0])) {
+                        $sender->sendMessage(TextFormat::GOLD . "Setting warp");
+                        $leveled = $player->getLevel()->getFolderName();
+                        $z = $player->getZ();
+                        $x = $player->getX();
+                        $y = $player->getY();
+                        $warp = $args[0];
+                        $config = $plugin->getConfig();
+                        $config->set([$leveled, $x, $y, $z, "entry 2"], $warp);
+                        $config->save();
+                        $sender->sendMessage(TextFormat::GOLD . "Warp set at: $leveled, $x, $y, $z");
+                        return true;
+                    } else {
+                        $sender->sendMessage(TextFormat::RED . "Incorrect usage or privlages!");
+                        return false;
+                    }
+                }
+
+                if ($sender->hasPermission("emerald.warp")) {
+                    if (strtolower($command->getName()) == "emeraldwarp") {
+                        if ($sender instanceof Player) {
+                            if (isset($args[0])) {
+                                $warpp = $args[0];
+                                $configg = $plugin->getConfig();
+                                $configg->get([$leveled, $x, $y, $z, "entry 2"], $warpp);
+                                $sender->sendMessage(TextFormat::GOLD . "Warping!");
+                                $player->teleport(new Position($leveled, $x, $y, $z));
+                                return true;
+                            } else {
+                                $sender->sendMessage(TextFormat::RED . "Not set or no perms!");
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
