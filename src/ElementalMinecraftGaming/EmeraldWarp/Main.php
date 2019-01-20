2fee7034eac91a67770967f9f2e52ae909485c9c
MDC <mathewpsutherland@gmail.com>
2019-01-20 19:40:59 +0000

    Fix #1

--- src/ElementalMinecraftGaming/EmeraldWarp/Main.php
+++ src/ElementalMinecraftGaming/EmeraldWarp/Main.php
@@ -13,6 +13,8 @@ use pocketmine\event\Listener;
 use pocketmine\command\utils\InvalidCommandSyntaxException;
 use pocketmine\event\TranslationContainer;
 use pocketmine\utils\TextFormat;
+use pocketmine\level\{Level,Position,ChunkManager};
+use pocketmine\Server;
 
 class Main extends PluginBase implements listener {
 
@@ -31,18 +33,18 @@ class Main extends PluginBase implements listener {
                 if ($sender instanceof Player) {
                     if (isset($args[0])) {
                         $sender->sendMessage(TextFormat::GOLD . "Setting warp");
-                        $leveled = $sender->getLevel()->getFolderName();
+                        $a = $sender->getLevel()->getFolderName();
                         $z = $sender->getZ();
                         $x = $sender->getX();
                         $y = $sender->getY();
                         $warp = $args[0];
-                        $config = $plugin->getConfig();
-                        $config->set([$leveled, $x, $y, $z, "entry 2"], $warp);
+                        $config = $this->getConfig();
+                        $config->set($warp, [$a, $x, $y, $z]);
                         $config->save();
-                        $sender->sendMessage(TextFormat::GOLD . "Warp set at: $leveled, $x, $y, $z");
+                        $sender->sendMessage(TextFormat::GOLD . "Warp $warp set at: $a, $x, $y, $z");
                         return true;
                     } else {
-                        $sender->sendMessage(TextFormat::RED . "Warp creation failed!");
+                        $sender->sendMessage(TextFormat::RED . "Warp creation failed: No perms!");
                         return false;
                     }
                 }
@@ -53,16 +55,56 @@ class Main extends PluginBase implements listener {
             if (strtolower($command->getName()) == "emeraldwarp") {
                 if ($sender instanceof Player) {
                     if (isset($args[0])) {
-                        $warpp = $args[0];
-                        $configg = $plugin->getConfig();
-                        $configg->get([$leveled, $x, $y, $z, "entry 2"], $warpp);
-                        $sender->sendMessage(TextFormat::GOLD . "Warping!");
-                        $sender->teleport(new Position($leveled, $x, $y, $z));
+                        $warp = $args[0];
+                        $config = $this->getConfig();
+                        $data = $config->get($args[0]);
+                        if (is_array($data)) {
+                            $land = $data[0];
+                            $x = $data[1];
+                            $y = $data[2];
+                            $z = $data[3];
+                        }
+                        $sender->sendMessage(TextFormat::GOLD . "Warping $warp at $land, $x, $y, $z!");
+                        $sender->teleport(new Position($x, $y, $z, $this->getServer()->getLevelByName($land))); //
                         return true;
                     } else {
                         $sender->sendMessage(TextFormat::RED . "Warp failed: Not set or no perms!");
                         return false;
                     }
+                }
+            }
+        }
+
+	if ($sender->hasPermission("emerald.delwarp")) {
+            if (strtolower($command->getName()) == "emeraldelwarp") {
+                if ($sender instanceof Player) {
+                    if (isset($args[0])) {
+                        $warp = $args[0];
+                        $config = $this->getConfig();
+			unset($config->$warp);
+			$this->getConfig()->save();
+                        return true;
+                    } else {
+                        $sender->sendMessage(TextFormat::RED . "Delete failed: Not set or no perms!");
+                        return false;
+                    }
+                }
+            }
+        }
+
+        if ($sender->hasPermission("emerald.listw")) {
+            if (strtolower($command->getName()) == "emeraldlistwarps") {
+                if ($sender instanceof Player) {
+                    $config = $this->getConfig();
+                    $list = $this->config->getAll();
+                    $warps = implode(", ", $list);
+                    $sender->sendMessage("----------");
+                    $sender->sendMessage($warps);
+		    $sender->sendMessage("----------");
+                    return true;
+                } else {
+                    $sender->sendMessage(TextFormat::RED . "Warp List failed: Not set or no perms!");
+                    return false;
                 }
             }
         }
